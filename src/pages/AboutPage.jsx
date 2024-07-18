069db01e90ee950d39620dd1c31cb2e088f68ca3
RApostolJR <romuloapostol@gmail.com>
2024-07-17 17:09:37 -0700

    updated description in AboutPage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,8 +6,11 @@ export default function AboutPage() {
             <img src={myHeadshot} />
             <h1>About Me</h1>
             <p>
-                My name is Romulo Apostol, and I am a software engineer with a passion for problem-solving and creativity. 
-                I have been working remotely for the past 5 years, focusing primarily on web development and design.
+                My name is Romulo Apostol, and I have a bachelor's degree in International Security and Conflict Resolution.
+                I have always had an interest in computers whether it is building them or learning how to code. 
+                I have been taking a Computer Programming Course at UC Berkley Coding Bootcamp.
+                I have been learning how to code and have created multiple projects by myself and projects with my classmates. I am a hard worker
+                and I am always seeking opportunities to learn and grow. 
             </p>
         </section>
     );
