9de843e7bad81f15cd69f189a626e0ec1c834a16
RApostolJR <romuloapostol@gmail.com>
2024-07-18 18:06:58 -0700

    updated some looks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,14 @@ import linkedinIcon from '../assets/images/linkedin-icon.png';
 
 const Footer = () => (
   <footer className="footer">
+    <hr/>
     <a href="https://github.com/rapostoljr" target="_blank" rel="noopener noreferrer" className="icon">
       <img src={githubIcon} alt="GitHub" className="icon-image" />
     </a>
     <a href="https://www.linkedin.com/in/romulo-apostol-7886041b3/" target="_blank" rel="noopener noreferrer" className="icon">
       <img src={linkedinIcon} alt="LinkedIn" className="icon-image" />
     </a>
+    <hr/>© 2024 Romulo Apostol Jr., LLC All Rights Reserved.
   </footer>
 );
 
--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import resume from '../assets/files/RESUME.pdf'
+import pdf from '../assets/images/pdf.png';
+
 
 
 export default function ResumePage() {
     return (
         <section className="resume">
             <h2>Resume</h2>
-            <a href={resume} download="Romulo_Apostol_Resume.pdf">Download</a>
+            <a href={resume} download="Romulo_Apostol_Resume.pdf">
+                <img src={pdf} alt="pdf"/>Download</a>
             <iframe
                 src={resume}
                 width="100%"
