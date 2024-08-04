83395b850aa018a77f27c3b7620f53b531e0d13a
RApostolJR <romuloapostol@gmail.com>
2024-08-04 11:15:39 -0700

    updated resume, changed description in aboutpage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -6,11 +6,12 @@ export default function AboutPage() {
             <img src={myHeadshot} />
             <h1>About Me</h1>
             <p>
-                My name is Romulo Apostol, and I have a bachelor's degree in International Security and Conflict Resolution.
-                I have always had an interest in computers whether it is building them or learning how to code. 
-                I have been taking a Computer Programming Course at UC Berkley Coding Bootcamp.
-                I have been learning how to code and have created multiple projects by myself and projects with my classmates. I am a hard worker
-                and I am always seeking opportunities to learn and grow. 
+                My name is Romulo Apostol Jr, and I hold a Bachelor's degree in International Security and Conflict Resolution. 
+                My longstanding interest in technology, particularly in computer hardware and coding, has led me to pursue further 
+                education in this field. I have graduated from the Full Stack Coding Bootcamp at UC Berkeley Extension, 
+                where I have been actively developing my coding skills through individual and collaborative projects. As a 
+                dedicated and diligent learner, I am continually seeking opportunities to enhance my knowledge and contribute 
+                meaningfully to the field.
             </p>
         </section>
     );
